cb7f729bf28d775efedaa81d25dd6ea5771b512a
kongdexing798 <15025340606@163.com>
2018-10-17 15:56:54 +0800

    完善jdbc连接数据库

--- src/com/pmo/Jdbc.java
+++ src/com/pmo/Jdbc.java
@@ -1,29 +1,39 @@
 package com.pmo;
 
 
-import com.mysql.jdbc.Connection;
 
+import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 
 /**
  * Created by ASUS on 2018/10/17.
  */
 public class Jdbc {
-      public static void main(String[] args) {
+      public static void main(String[] args) throws Exception{
             // JDBC开发步骤
           //1、注册驱动，告诉jvm使用的是哪一个数据库驱动
             Class.forName("com.mysql.jdbc.Driver");
           //2、获取连接
           String url="jdbc:mysql://localhost:3306/test";
           String username="root";
-          String password="1234";
+          String password="root";
           Connection conn= DriverManager.getConnection(url, username, password);
           //3、获得语言执行平台，?通过连接对象获取对SQL语句的执行者对象
+          String sql ="select 1";
+          PreparedStatement prt=conn.prepareStatement(sql);
           //4、执行sql语句
+          ResultSet rs = prt.executeQuery();
           //5、处理结果
+          while (rs.next()){
+              System.out.println(rs.getString(1));
+          }
           //6、释放资源
-
+          rs.close();
+          prt.close();
+          conn.close();
 
           }
 }
